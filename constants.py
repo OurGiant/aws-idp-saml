7cdf9f8f2e29681d7a438efd2729dca299234186
Ryan Leach <rleach@national.aaa.com>
2024-05-13 13:50:17 -0400

    fix test for sec labels

--- constants.py
+++ constants.py
@@ -51,7 +51,7 @@ gecko_local_file = {
 
 security_method_labels = {
     "enter_code": "[aria-label='Select Okta FastPass.']",
-    "push_notify": "[aria-label='Select Okta FastPass.']",
-    "fast_pass": "[aria-label='Select Okta FastPass.']"
+    "push_notify": "[aria-label='Select to get a push notification to the Okta Verify app.']",
+    "fast_pass": "[aria-label='Select to enter a code from the Okta Verify app.']"
 }
 
